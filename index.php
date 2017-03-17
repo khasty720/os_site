b26cf5bf1d4a27ae71be34da0d691ba7852c5a9a
root <root@ip-172-31-30-252.eu-west-2.compute.internal>
2017-03-17 01:07:07 +0000

    init commit

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>OS Designs</title>
+
+    <!-- Bootstrap -->
+    <link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container body-container">
+      <div class="left_rail">
+
+        <div class="row">
+          <div class="col-xs-12">
+      	     <div class="logo"><a href="#" data-scrollto="home">OS Designs<span>London</span></a></div>
+           </div>
+         </div>
+
+         <div class="row">
+           <div class="col-xs-12">
+              <ul class="_nav">
+              	<li><a href="#" data-scrollto="services">Services</a></li>
+              	<li><a href="#" data-scrollto="about">About Us</a></li>
+              	<li><a href="#" data-scrollto="portfolio">Portfolio</a></li>
+              	<li><a href="#" data-scrollto="contact">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="copyright">
+              	<div class="social">
+              	  <a href="https://www.facebook.com/OsDesignsLimited" class="external"><i class="fa fa-2x fa-facebook-square"></i></a>
+                  <a href="https://uk.linkedin.com/in/oz-özge-lancaster-74770826" class="external"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                  <a href="https://twitter.com/ozlancaster" class="external"><i class="fa fa-2x fa-twitter-square"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+      </div>
+
+    <div class="main">
+
+    	<section id="section_home" class="home">
+        	<div class="quote">
+            	"If I were asked to say what is at once the most important production of Art and the thing most longed for, I should answer
+                <span class="script">A Beautiful House</span>
+                <span class="author">William Morris</span>
+            </div>
+        </section>
+
+    	<section id="section_services" class="services">
+      	<div class="_inner">
+              <h2>Services</h2>
+              <h3>Interior Designs Services</h3>
+              <p>OS Designs provides bespoke solutions to renovation projects. Through close collaboration with our clients, tailored concept boards detailing the mood of the space including fabrics, furniture and room layouts are produced. A complete design package including crockery, linen and accessories can be produced taking the strain away from our clients and allowing them to move into a beautifully furnished space. OS Designs works with appointed contractors and the client to reflect each client’s taste in the most beautiful way.</p>
+              <h3>Architectural Design</h3>
+              <p>OS Designs has notable experience working alongside planning officials to gain planning permission for listed buildings and heritage sites. The team can take on complete project management of a full-scale build, producing full technical architectural drawings. The team works closely with building contractors and structural building professionals to ensure a successful outcome where functionality meets precise design details resulting in a beautiful, bespoke finish to any project. </p>
+              <h3>Project Management</h3>
+              <p>Through in depth knowledge of the industry, key skills and great experience, OS Designs can achieve the goals of each individual project, whether it’s a complete refurbishment and internal redesign to an architectural rebuild. </p>
+              <p>Keeping clients up to date is essential for the smooth running of all projects; cost analysis, procurement and budgeting schedules as well as time frame programmes are always produced as part of the design package. </p>            	<p>OS Designs manages each project from the beginning stages of planning and design development, through to the end where the design installation delivers the envisioned unique, elegant interiors with attention to detail. This includes sourcing beautiful furniture and fabrics as well as designing bespoke joinery, turning our clients’ visions into reality.</p>
+  	    </div>
+        </section>
+
+      	<section id="section_about" class="about">
+        	<div class="_inner">
+                <h2>About Us</h2>
+                <p>OS Designs carefully manages each project from beginning to end, offering a complete design service and project management throughout. Oz Lancaster is the Design Director & Founder of the studio, and together with an experienced team of designers she provides beautifully unique and functional designs for each individual client. OS Designs places great emphasis on maintaining strong client relations, tailoring each project to the specific needs and vision of the individual, creating exceptional, elegant interiors for private and commercial clients.</p>
+
+                <p>The studio has an international portfolio including large-scale architectural design and renovation for listed buildings in central London, interior design for commercial and residential projects across London, Buckinghamshire and Oxfordshire as well as many projects in the Middle East. </p>
+
+                <h3>Oz Lancaster, Design Director &amp; Founder</h3>
+                <p>Design Director, Oz Lancaster (RIBA affiliate member), is the leading force behind the company. Oz started applying her talents during her Interior Architecture and Design degree, which was soon followed with an MA in Furniture Design from Buckinghamshire Chilterns University College – the leading Furniture Design program in the UK. Her diverse background allows her to look beyond just the furnishing of a room; she has a strong architectural background allowing her to design and further develop the architectural and structural elements of the projects undertaken by the company.</p>
+                <p>Oz has an in-depth understanding of global cultures and trends due to growing up in Cyprus and continuing her studies in the UK. Oz has successfully worked on the most exclusive developments not only in London but across the continent and in the Middle East. Her ongoing success can be most recently seen within the exciting and innovative world of aviation and yacht design whereby her exterior and interior designs are revitalising the market place. </p>
+  		    </div>
+        </section>
+
+      	<section id="section_portfolio" class="portfolio">
+
+          <div class="container gallery">
+              <div class="row">
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="img/portfolio/1.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 2" href="#"><img class="thumbnail img-responsive" src="img/portfolio/2.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 3" href="#"><img class="thumbnail img-responsive" src="img/portfolio/3.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 4" href="#"><img class="thumbnail img-responsive" src="img/portfolio/4.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="img/portfolio/5.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="img/portfolio/6.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 7" href="#"><img class="thumbnail img-responsive" src="img/portfolio/7.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 8" href="#"><img class="thumbnail img-responsive" src="img/portfolio/8.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 9" href="#"><img class="thumbnail img-responsive" src="img/portfolio/9.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="img/portfolio/10.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 11" href="#"><img class="thumbnail img-responsive" src="img/portfolio/11.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 12" href="#"><img class="thumbnail img-responsive" src="img/portfolio/12.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 13" href="#"><img class="thumbnail img-responsive" src="img/portfolio/13.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 14" href="#"><img class="thumbnail img-responsive" src="img/portfolio/14.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 15" href="#"><img class="thumbnail img-responsive" src="img/portfolio/15.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 16" href="#"><img class="thumbnail img-responsive" src="img/portfolio/16.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 17" href="#"><img class="thumbnail img-responsive" src="img/portfolio/17.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 18" href="#"><img class="thumbnail img-responsive" src="img/portfolio/18.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 19" href="#"><img class="thumbnail img-responsive" src="img/portfolio/19.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 20" href="#"><img class="thumbnail img-responsive" src="img/portfolio/20.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 21" href="#"><img class="thumbnail img-responsive" src="img/portfolio/21.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 22" href="#"><img class="thumbnail img-responsive" src="img/portfolio/22.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 23" href="#"><img class="thumbnail img-responsive" src="img/portfolio/23.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 24" href="#"><img class="thumbnail img-responsive" src="img/portfolio/24.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 25" href="#"><img class="thumbnail img-responsive" src="img/portfolio/25.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 26" href="#"><img class="thumbnail img-responsive" src="img/portfolio/26.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 27" href="#"><img class="thumbnail img-responsive" src="img/portfolio/27.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 28" href="#"><img class="thumbnail img-responsive" src="img/portfolio/28.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 29" href="#"><img class="thumbnail img-responsive" src="img/portfolio/29.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 30" href="#"><img class="thumbnail img-responsive" src="img/portfolio/30.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 31" href="#"><img class="thumbnail img-responsive" src="img/portfolio/31.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 32" href="#"><img class="thumbnail img-responsive" src="img/portfolio/32.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 33" href="#"><img class="thumbnail img-responsive" src="img/portfolio/33.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 34" href="#"><img class="thumbnail img-responsive" src="img/portfolio/34.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 35" href="#"><img class="thumbnail img-responsive" src="img/portfolio/35.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 36" href="#"><img class="thumbnail img-responsive" src="img/portfolio/36.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 37" href="#"><img class="thumbnail img-responsive" src="img/portfolio/37.jpg"></a></div>
+                <div class="col-lg-2 col-sm-3 col-xs-6"><a title="Image 38" href="#"><img class="thumbnail img-responsive" src="img/portfolio/38.jpg"></a></div>
+              </div>
+          </div>
+
+        <!-- Modal Carousel -->
+          <div class="modal fade" id="image-modal" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+
+              	<div class="modal-body">
+            		  <div id="modal-carousel" class="carousel slide container" data-interval="false">
+                    <div class="carousel-inner">
+                      <!-- Popualted with thumbnails -->
+                    </div>
+
+                    <a class="left carousel-control" href="#modal-carousel" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="right carousel-control" href="#modal-carousel" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                  </div>
+              	</div>
+
+             </div>
+            </div>
+          </div>
+
+
+          </section> <!-- / portfolio -->
+
+    	<section id="section_contact" class="contact">
+        	<div class="_inner">
+                <!--h2>Contact Us</h2-->
+                <div class="form_outer">
+                    <div class="_info">
+                        <p>
+                            OS Designs Ltd.<br/>
+                            23-24 Hyde Park Gardens<br/>
+                            London<br/>
+                            W2 2LY<br/>
+                            <a href="mailto:info@osdesigns.co.uk">info@osdesigns.co.uk</a>
+                        </p>
+                        <p>Tel +44 (0) 207 352 6323</p>
+                    </div>
+                    <div class="_form">
+                        <form action="#">
+                            <div>
+                                <div class="_row">
+                                    <label for="contact_fullname">Name:</label>
+                                    <input class="form-control" type="text" id="contact_fullname" name="fullname" placeholder="Enter Name" />
+                                </div>
+                                <div class="_row">
+                                    <label for="contact_email">Email:</label>
+                                    <input class="form-control" type="text" id="contact_email" name="email" placeholder="Enter Email" />
+                                </div>
+                                <div class="_row">
+                                    <label for="contact_email">Message:</label>
+                                    <textarea class="form-control" id="contact_message" name="message" placeholder="Enter Your Message"></textarea>
+                                </div>
+                                <div class="_row">
+                                    <button type="button" class="btn btn-default">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+			        </div>
+        </section>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/vendor/jquery.easing.1.3.js"></script>
+    <script src="js/vendor/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script src="js/main.js"></script>
+  </div>
+  </body>
+</html>
