d58d44542affdde89813c4a1996d1ae8fee353ce
Kyle Hasty <khasty1@jh.edu>
2017-03-29 23:26:29 -0400

    Updated nav color

--- index.php
+++ index.php
@@ -37,27 +37,19 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+                <li><a href="#" data-scrollto="services">Services</a></li>
+                <li><a href="#" data-scrollto="about">About Us</a></li>
+                <li><a href="#" data-scrollto="projects">Projects</a></li>
+                <li><a href="#" data-scrollto="portfolio">Portfolio</a></li>
+                <li><a href="#" data-scrollto="contact">Contact Us</a></li>
               </ul>
+              <!--
               <ul class="nav navbar-nav navbar-right">
                 <li><a href="../navbar/">Default</a></li>
                 <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
                 <li><a href="../navbar-fixed-top/">Fixed top</a></li>
               </ul>
+              -->
             </div><!--/.nav-collapse -->
           </div>
         </nav>
